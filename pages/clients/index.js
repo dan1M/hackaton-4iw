1b0fd559ce04ef1663c82fe7f4e261601d2825d8
dan1M <maneadaniel555@gmail.com>
2023-07-25 23:07:55 +0200

    card component, edit client form,  crud client

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,42 +1,47 @@
-import Button from "@/components/Button";
-import Card from "@/components/Card";
-import { useSessionContext } from "@supabase/auth-helpers-react";
-import { useState } from "react";
-import CustomModal from "@/components/CustomModal";
+import Button from '@/components/Button';
+import Card from '@/components/Card';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import { useEffect, useState } from 'react';
+import CustomModal from '@/components/CustomModal';
+import Sidebar from '@/components/Sidebar';
 
 const Clients = () => {
-  const [displayNewUser, setDisplayNewUser] = useState(false);
-
-  const [showModal, setShowModal] = useState(false);
-
   const { supabaseClient } = useSessionContext();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [clients, setClients] = useState([]);
 
   const [formData, setFormData] = useState({
-    name: "",
+    name: '',
   });
 
   const customStyles = {
     content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      borderRadius: "8px",
-      //   border: "1px solid #ccc",
-      //   boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
-      padding: "20px",
-      maxWidth: "400px",
-      backgroundColor: "#282B2A",
-      //   backgroundColor: "#282B2A", // Utilisez la variable CSS personnalisée "var(--primary)" pour la couleur de fond
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      borderRadius: '8px',
+      padding: '20px',
+      border: 'none',
+      maxWidth: '400px',
+      backgroundColor: '#282B2A',
     },
     overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.6)",
+      backgroundColor: 'rgba(0, 0, 0, 0.6)',
       zIndex: 1000,
     },
+  };
+
+  useEffect(() => {
+    fetchClients();
+  }, []);
+
+  const fetchClients = async () => {
+    const { data } = await supabaseClient.from('clients').select('*');
+    setClients(data);
   };
 
   const handleOpenModal = () => {
@@ -45,162 +50,108 @@ const Clients = () => {
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    console.log("ici");
   };
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevData => ({
+    setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
   };
 
-  const handleCreateUser = async e => {
+  const handleCreateUser = async (e) => {
     e.preventDefault();
     const { error } = await supabaseClient
-      .from("clients")
+      .from('clients')
       .insert({ name: formData.name });
     handleCloseModal();
+    fetchClients();
   };
 
   return (
-    <main>
-      <button onClick={handleOpenModal}>Open Modal</button>
+    <main className='p-4'>
+      <Button text='Ajouter un client' onClick={handleOpenModal} />
 
       <CustomModal
         isOpen={isModalOpen}
         onRequestClose={handleCloseModal}
         styles={customStyles}
       >
-        <div className="relative w-full max-w-md max-h-full">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div className='relative w-full max-w-md max-h-full'>
+          <div className='relative'>
             <button
-              type="button"
-              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              type='button'
+              className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white'
               onClick={handleCloseModal}
             >
               <svg
-                className="w-3 h-3"
-                //   aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
+                className='w-3 h-3'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 14 14'
               >
                 <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
                 />
               </svg>
-              <span className="sr-only">Close modal</span>
+              <span className='sr-only'>Close modal</span>
             </button>
-            <div className="px-6 py-6 lg:px-8">
-              <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+            <div className='px-6 py-6 lg:px-8'>
+              <h3 className='mb-4 text-xl font-medium text-gray-900 dark:text-white'>
                 Ajouter un client
               </h3>
-              <form className="space-y-6" onSubmit={handleCreateUser}>
+              <form className='space-y-6' onSubmit={handleCreateUser}>
                 {
                   <div>
                     <label
-                      htmlFor="name"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      htmlFor='name'
+                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
                     >
                       Name
                     </label>
                     <input
-                      type="text"
-                      name="name"
-                      id="name"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      placeholder="Name"
+                      type='text'
+                      name='name'
+                      id='name'
+                      className='bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:placeholder-gray-400 dark:text-white'
+                      placeholder='Name'
                       onChange={handleChange}
                       required
                     />
                   </div>
-                  /*
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Mot de passe
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    required
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="first_name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Prénom
-                  </label>
-                  <input
-                    name="first_name"
-                    id="first_name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="John"
-                    required
-                  />
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="last_name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Nom
-                  </label>
-                  <input
-                    name="last_name"
-                    id="last_name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                    placeholder="Doe"
-                    required
-                  />
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="role"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Rôle
-                  </label>
-                  <select
-                    value={"dev"}
-                    id="role"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  >
-                    <option value="com">Commercial</option>
-                    <option value="rh">Ressource Humaines</option>
-                    <option value="dev">Consultant</option>
-                    <option value="mgr">Manager</option>
-                  </select>
-                </div> */
                 }
 
-                <button
-                  type="submit"
-                  className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Ajouter un client
-                </button>
+                <Button text='Ajouter un client' type='submit' />
               </form>
             </div>
           </div>
         </div>
       </CustomModal>
-      <Card />
+      <div className='flex flex-wrap'>
+        {clients?.map((client) => {
+          return (
+            <Card
+              key={client.id}
+              id={client.id}
+              title={client.name}
+              imageUrl={'next.svg'}
+              triggerFetch={(id) => {
+                if (id) {
+                  setClients(clients.filter((client) => client.id !== id));
+                } else {
+                  fetchClients();
+                }
+              }}
+              type='client'
+            />
+          );
+        })}
+      </div>
     </main>
   );
 };
