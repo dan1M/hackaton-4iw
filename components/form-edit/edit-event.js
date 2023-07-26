6cf8bd174a2851c3edb815f4f2256b1b0811eb07
Ady Masivi <adymasivi@MacBook-Air-de-Ady.local>
2023-07-26 11:32:14 +0200

    event

--- components/form-edit/edit-event.js
+++ components/form-edit/edit-event.js
@@ -1,3 +1,79 @@
-export default function FormEvent() {
-  return <></>;
+
+import { useEffect, useState } from 'react';
+import Button from '../Button';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+
+export default function FormEvent({ id, triggerFetch }) {
+  const { supabaseClient } = useSessionContext();
+  const [formData, setFormData] = useState({
+    name: '',
+  });
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const { data, error } = await supabaseClient
+      .from('events')
+      .select('*')
+      .eq('id', id)
+      .single();
+
+    if (data) {
+      setFormData({ name: data.name });
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const updateEvent = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabaseClient
+      .from('events')
+      .update({ name: formData.name })
+      .eq('id', id)
+      .select();
+
+    if (data) {
+      triggerFetch();
+    }
+  };
+
+  return (
+    <div className='px-6 py-6 lg:px-8'>
+      <h3 className='mb-4 text-xl font-medium text-gray-900 dark:text-white'>
+        Modifer un évènement
+      </h3>
+      <form className='space-y-6' onSubmit={updateEvent}>
+        {
+          <div>
+            <label
+              htmlFor='name'
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+            >
+              Nom
+            </label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              className='bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:placeholder-gray-400 dark:text-white'
+              placeholder='Name'
+              onChange={handleChange}
+              value={formData.name}
+              required
+            />
+          </div>
+        }
+
+        <Button text='Modifier un évènement' type='submit' />
+      </form>
+    </div>
+  );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,145 +1,130 @@
-import { useUser, useSessionContext } from '@supabase/auth-helpers-react';
-import { useState, useEffect } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 import Button from '@/components/Button';
+import Card from '@/components/Card';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import { useEffect, useState } from 'react';
+import CustomModal from '@/components/CustomModal';
 
-const validationSchema = Yup.object().shape({
-  name: Yup.string().required('Name is required'),
-  place: Yup.string().required('place is required'),
-});
-
-const EventPage = () => {
+const Event = () => {
   const { supabaseClient } = useSessionContext();
-  const user = useUser();
-  const [data, setData] = useState([]);
-  const [editEventId, setEditEventId] = useState(null);
 
-  const fetchEventData = async () => {
-    try {
-      const { data } = await supabaseClient.from('events').select('*');
-      setData(data);
-    } catch (error) {
-      console.error('Error fetching event data:', error);
-    }
-  };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [evenements, setEvenements] = useState([]);
+
+  const [formData, setFormData] = useState({
+    name: '',
+  });
 
   useEffect(() => {
-    if (user) {
-      fetchEventData();
-    }
-  }, [user, supabaseClient]);
+    fetchEvents();
+  }, []);
 
-  const handleCreateEvent = async (values) => {
-    try {
-      const { data, error } = await supabaseClient.from('events').insert(values);
-      if (error) {
-        console.error('Error creating event:', error);
-      } else {
-        console.log('Event created successfully:', data);
-        fetchEventData();
-      }
-    } catch (error) {
-      console.error('Error creating event:', error);
-    }
+  const fetchEvents = async () => {
+    const { data } = await supabaseClient.from('events').select('*');
+    setEvenements(data);
   };
 
-  const handleEditEvent = (eventId) => {
-    setEditEventId(eventId);
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
   };
 
-  const handleUpPlaceEvent = async (values) => {
-    try {
-      const { data, error } = await supabaseClient
-        .from('events')
-        .update(values)
-        .eq('id', editEventId);
-      if (error) {
-        console.error('Error updating event:', error);
-      } else {
-        console.log('Event updated successfully:', data);
-        setEditEventId(null);
-        fetchEventData();
-      }
-    } catch (error) {
-      console.error('Error updating event:', error);
-    }
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
   };
 
-  const handleDeleteEvent = async (eventId) => {
-    try {
-      const { error } = await supabaseClient.from('events').delete().eq('id', eventId);
-      if (error) {
-        console.error('Error deleting event:', error);
-      } else {
-        console.log('Event deleted successfully');
-        fetchEventData();
-      }
-    } catch (error) {
-      console.error('Error deleting event:', error);
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleCreateEvent = async (e) => {
+    e.preventDefault();
+    const { error } = await supabaseClient.from('events').insert({ name: formData.name });
+    handleCloseModal();
+    fetchEvents();
   };
 
   return (
-    <>
-      {data.map((event) => (
-        <div key={event.id}>
-          {editEventId === event.id ? (
-            <Formik
-              initialValues={{ name: event.name, place: event.place }}
-              validationSchema={validationSchema}
-              onSubmit={(values) => {
-                handleUpPlaceEvent({ ...values, id: event.id });
-              }}
+    <main className='p-4'>
+      <Button text='Ajouter un évènement' onClick={handleOpenModal} />
+
+      <CustomModal isOpen={isModalOpen} onRequestClose={handleCloseModal}>
+        <div className='relative w-full max-w-md max-h-full'>
+          <div className='relative'>
+            <button
+              type='button'
+              className='absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white'
+              onClick={handleCloseModal}
             >
-              <Form>
+              <svg
+                className='w-3 h-3'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 14 14'
+              >
+                <path
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
+                />
+              </svg>
+              <span className='sr-only'>Close modal</span>
+            </button>
+            <div className='px-6 py-6 lg:px-8'>
+              <h3 className='mb-4 text-xl font-medium text-gray-900 dark:text-white'>
+                Ajouter un évènement
+              </h3>
+              <form className='space-y-6' onSubmit={handleCreateEvent}>
                 <div>
-                  <label htmlFor='name'>Name:</label>
-                  <Field type='text' name='name' />
-                  <ErrorMessage name='name' component='div' className='error' />
+                  <label
+                    htmlFor='name'
+                    className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                  >
+                    Name
+                  </label>
+                  <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    className='bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:placeholder-gray-400 dark:text-white'
+                    placeholder='Name'
+                    onChange={handleChange}
+                    required
+                  />
                 </div>
-                <div>
-                  <label htmlFor='place'>Place:</label>
-                  <Field type='text' name='place' />
-                  <ErrorMessage name='place' component='div' className='error' />
-                </div>
-                <Button type='submit' text='Save' />
-              </Form>
-            </Formik>
-          ) : (
-            <>
-              <h2>{event.name}</h2>
-              <p>{event.place}</p>
-              <button onClick={() => handleEditEvent(event.id)}>Edit</button>
-              <button onClick={() => handleDeleteEvent(event.id)}>Delete</button>
-            </>
-          )}
+
+                <Button text='Ajouter un évènement' type='submit' />
+              </form>
+            </div>
+          </div>
         </div>
-      ))}
-      <Formik
-        initialValues={{ name: '', place: '' }}
-        validationSchema={validationSchema}
-        onSubmit={(values, { resetForm }) => {
-          handleCreateEvent(values);
-          resetForm();
-        }}
-      >
-        <Form>
-          <div>
-            <label htmlFor='name'>Name:</label>
-            <Field type='text' name='name' />
-            <ErrorMessage name='name' component='div' className='error' />
-          </div>
-          <div>
-            <label htmlFor='place'>Place:</label>
-            <Field type='text' name='place' />
-            <ErrorMessage name='place' component='div' className='error' />
-          </div>
-          <Button type='submit' text='Create' />
-        </Form>
-      </Formik>
-    </>
+      </CustomModal>
+      <div className='flex flex-wrap'>
+        {evenements?.map((evenement) => {
+          return (
+            <Card
+              key={evenement.id}
+              id={evenement.id}
+              title={evenement.name}
+              imageUrl={'next.svg'}
+              triggerFetch={(id) => {
+                if (id) {
+                  setEvenements(evenements.filter((evenement) => evenement.id !== id));
+                } else {
+                  fetchEvents();
+                }
+              }}
+              type='event'
+            />
+          );
+        })}
+      </div>
+    </main>
   );
 };
 
-export default EventPage;
+export default Event
