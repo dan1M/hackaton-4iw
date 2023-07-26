9d07758fe4b0f3082ae088e8988e20e0c17c16e6
Ady Masivi <adymasivi@MacBook-Air-de-Ady.local>
2023-07-26 11:35:56 +0200

    update event

--- components/form-edit/edit-event.js
+++ components/form-edit/edit-event.js
@@ -57,7 +57,7 @@ export default function FormEvent({ id, triggerFetch }) {
               htmlFor='name'
               className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
             >
-              Nom
+              Nomxz
             </label>
             <input
               type='text'
