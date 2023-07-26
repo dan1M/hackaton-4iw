9d173a0da3f461c24c0f38ac69708a2703b7f3c7
jugo92 <jugzek92@outlook.fr>
2023-07-26 22:15:47 +0200

    update component Button

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,19 @@
-const Button = (props) => {
-  const { text = 'Soumettre', type = 'default' } = props;
+import { defaultConfig } from "next/dist/server/config-shared";
+
+const Button = props => {
+  const { text = "Soumettre", type = "default" } = props;
+  const defaultStyle =
+    "  font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:text-white dark:bg-primary ";
   return (
     <button
       className={
-        'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'
+        type === "danger"
+          ? defaultStyle + "dark:bg-secondary dark:hover:bg-secondary"
+          : type === "active"
+          ? defaultStyle + "dark:text-white dark:bg-green-800"
+          : type === "transparent"
+          ? defaultStyle + "dark:text-white"
+          : defaultStyle
       }
       {...props}
     >
