d9b562af06dcdd889177030894e0822a6f9177cb
jugo92 <jugzek92@outlook.fr>
2023-07-27 10:58:07 +0200

    update styles button/card

--- components/Button.js
+++ components/Button.js
@@ -3,16 +3,15 @@ import { defaultConfig } from "next/dist/server/config-shared";
 const Button = props => {
   const { text = "Soumettre", type = "default" } = props;
   const defaultStyle =
-    "  font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:text-white dark:bg-primary ";
+    "  font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:text-white bg-transparent border border-gray-400 hover:bg-carbon-blue ";
   return (
     <button
       className={
         type === "danger"
-          ? defaultStyle + "dark:bg-secondary dark:hover:bg-secondary"
+          ? defaultStyle + "dark:bg-secondary dark:hover:bg-red-700 border-none"
           : type === "active"
-          ? defaultStyle + "dark:text-white dark:bg-green-800"
-          : type === "transparent"
-          ? defaultStyle + "dark:text-white"
+          ? defaultStyle +
+            "dark:bg-carbon-green dark:hover:bg-carbon-green border-none"
           : defaultStyle
       }
       {...props}
--- components/Card.js
+++ components/Card.js
@@ -19,6 +19,7 @@ function Card({
 }) {
   const [displayModalDelete, setDisplayModalDelete] = useState(false);
   const [displayModalEdit, setDisplayModalEdit] = useState(false);
+  const [showDropdown, setShowDrown] = useState(false);
   const { supabaseClient } = useSessionContext();
 
   const customStyles = {
@@ -85,10 +86,66 @@ function Card({
     }
   };
 
+  const handleShowDropdown = () => {
+    setShowDrown(!showDropdown);
+  };
+
   return (
     <>
-      <div className="w-full max-w-xs m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-zinc-500 dark:border-gray-700">
-        <div className="flex justify-end px-4 pt-4"></div>
+      <div className="w-full max-w-xs m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-carbon-blue dark:border-gray-700">
+        <div className="flex justify-end px-4 pt-4 relative ">
+          <button
+            onClick={handleShowDropdown}
+            id="dropdownButton"
+            data-dropdown-toggle="dropdown"
+            className="inline-block text-gray-500 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+            type="button"
+          >
+            <span className="sr-only">Open dropdown</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 16 3"
+            >
+              <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+            </svg>
+          </button>
+          {showDropdown && (
+            <div
+              id="dropdown"
+              className=" absolute right-12 z-10 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-primary"
+            >
+              <ul className="py-2" aria-labelledby="dropdownButton">
+                {type !== "user" &&
+                  title !== "Parler" &&
+                  title !== "Visiter Profil" && (
+                    <>
+                      <li>
+                        <a
+                          onClick={() => setDisplayModalEdit(true)}
+                          href="#"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Edit
+                        </a>
+                      </li>
+                    </>
+                  )}
+                <li>
+                  <a
+                    onClick={() => setDisplayModalDelete(true)}
+                    href="#"
+                    className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600  dark:hover:text-white"
+                  >
+                    Delete
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
         <div className="flex flex-col items-center pb-4 ">
           {imageUrl && (
             <Image
@@ -106,21 +163,7 @@ function Card({
           <span className="text-sm text-gray-500 dark:text-gray-400">
             {subtitle}
           </span>
-          <div className="flex mt-4 space-x-3 md:mt-6">
-            {type !== "user" &&
-              title !== "Parler" &&
-              title !== "Visiter Profil" && (
-                <Button
-                  text="Modifier"
-                  onClick={() => setDisplayModalEdit(true)}
-                />
-              )}
-
-            <Button
-              text="Supprimer"
-              onClick={() => setDisplayModalDelete(true)}
-            />
-          </div>
+          <div className="flex mt-4 space-x-3 md:mt-6"></div>
         </div>
       </div>
       <CustomModal
